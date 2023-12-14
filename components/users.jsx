0556c53254f2b762595347e9af877a6a20263433
J4imes <jaimesmiguel73@gmail.com>
2023-12-13 22:35:46 -0500

    Ref

--- components/users.jsx
+++ components/users.jsx
@@ -1,23 +1,22 @@
 "use client"; 
+import Link from "next/link";
 async function Users({users}){
     return (
         <ul>
         {users.map(user => (
-          <li key={user.id} className="bg-slate-400 mb-2 p-4 rounded-md text-black flex">
-            <div>
-              <h5 className="font-bold">
-                {users.id} {user.first_name} {user.last_name}
-              </h5>
-              <p className="text-slate-300">
-                email: {user.email}
-              </p>
-            
-            </div>
-            <img src={user.avatar}/>
-          </li>
+          <Link key={user.id} href={"users${user.id}"}>
+            <li className="bg-slate-400 mb-2 p-4 rounded-md text-black flex justify-between" onClick={() => {alert(user.id)}}>
+        
+                <div>
+                    <h5 className="font-bold"> {users.id} {user.first_name} {user.last_name}</h5>
+                    <p className="text-slate-300"> email: {user.email} </p>
+                </div>
+        
+                <img src={user.avatar} className="rounded-full w-20"/>
+            </li>
+          </Link>
         ))}
       </ul>
-  
     )
 
 

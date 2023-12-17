9f5acdb98f8f6a5fb92b8cb7a7fa8f7bce6fc671
J4imes <jaimesmiguel73@gmail.com>
2023-12-17 00:56:42 -0500

    ref[page.jsx]

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,11 +1,26 @@
-function UsersPage({paranms}){   
-    console.log(paranms);
-    return <div>
-        <h1>
-            hola xd
-        </h1>
-    </div>
-
+async function getUser(id){
+    const res = await fetch(`https://reqres.in/api/users/${id}`);
+    const data = await res.json();
+    return data.data;
 }
 
-export default UsersPage
+
+// async function UsersPage({paranms}){   
+//     const user = await getUser(paranms.id);
+//     return <h1>{JSON.stringify(user)} </h1>
+// }
+// export default UsersPage
+
+
+
+async function UsersPage({ params }) {
+    const user = await getUser(params.id);
+    
+    return <div className="bg-slate-400 mb-2 p-10 rounded-md">
+        <img src={user.avatar} alt=""  className="m-auto  my-4"/>
+        <h3 className="font-bold text-3xl">{user.id} {user.first_name} {user.last_name}</h3>
+        <p className="font-bold text-slate-300"> email: {user.email}</p>
+    </div>
+  }
+  
+  export default UsersPage;
